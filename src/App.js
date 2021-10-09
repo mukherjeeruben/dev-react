b805625067ced667d206cb89052ce2747c0339c2
mukherjeeruben <rubenmukherjee@outlook.com>
2021-10-09 02:44:27 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Developement in Progress. We will be Live soon !
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dev-app-api.herokuapp.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go To API
         </a>
       </header>
     </div>
